627b3323c9addb4ef6b10193abc4ccab37dd630f
김민성 <kmsz5813@naver.com>
2021-12-11 14:29:05 +0900

    피자 스토어 아이템 화면 뿌리기 까지 완성

--- app/src/main/java/com/nepplus/pizzaorderappku_20211211/adapters/StoreAdapter.kt
+++ app/src/main/java/com/nepplus/pizzaorderappku_20211211/adapters/StoreAdapter.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.nepplus.pizzaorderappku_20211211.R
 import com.nepplus.pizzaorderappku_20211211.datas.StoreData
 
 class StoreAdapter(
-    val mContext : Context,
-    val resId : Int,
-    val mList : ArrayList<StoreData>
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StoreData>
 
 ) : ArrayAdapter<StoreData>(mContext,resId,mList) {
 
@@ -27,6 +30,16 @@ class StoreAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[ position ]
+
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
+
+        txtStoreName.text=data.name
+
+
+        Glide.with(mContext).load(data.logoURL).into(imgLogo)
 
         return row
 
