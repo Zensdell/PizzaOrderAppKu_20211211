86da269a9a5014359b2cbb40ae4f91b1fe0b3585
김민성 <kmsz5813@naver.com>
2021-12-11 15:19:59 +0900

    내 프로필 프래그먼트로 돌아와서 새 닉네임 반영

--- app/src/main/java/com/nepplus/pizzaorderappku_20211211/EditNicknameActivity.kt
+++ app/src/main/java/com/nepplus/pizzaorderappku_20211211/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.nepplus.pizzaorderappku_20211211
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,20 @@ class EditNicknameActivity : AppCompatActivity() {
 
 
 
-}
+    fun setupEvents() {
 
-    fun setupEvents(){
+        btnOk.setOnClickListener {
+
+            val inputNickname = edtNickname.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+
+
+        }
 
     }
 
@@ -26,3 +39,4 @@ class EditNicknameActivity : AppCompatActivity() {
     }
 
 
+}
--- app/src/main/java/com/nepplus/pizzaorderappku_20211211/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/nepplus/pizzaorderappku_20211211/fragment/MyProfileFragment.kt
@@ -1,13 +1,19 @@
 package com.nepplus.pizzaorderappku_20211211.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nepplus.pizzaorderappku_20211211.EditNicknameActivity
 import com.nepplus.pizzaorderappku_20211211.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
+
+    val REQ_FOR_NICKNAME = 1000
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,32 @@ class MyProfileFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        btnEditNickname.setOnClickListener {
+
+            val myIntent = Intent(requireContext(),EditNicknameActivity::class.java)
+            startActivityForResult(myIntent,REQ_FOR_NICKNAME )
+
+        }
+
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode ==REQ_FOR_NICKNAME) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val nickname = data!!.getStringExtra("nick")
+
+                txtNickName.text = nickname
+
+            }
+
+        }
+
+    }
+
 
 }
